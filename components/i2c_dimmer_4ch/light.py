69c5099ff2073383f287a9e6a2a96e530d3d3fd4
Tero Kivinen <kivinen@iki.fi>
2026-04-26 03:11:51 +0300

    .

--- components/i2c_dimmer_4ch/light.py
+++ components/i2c_dimmer_4ch/light.py
@@ -18,7 +18,7 @@ I2CDimmer4ChLightOutput = i2c_dimmer_4ch_ns.class_("I2CDimmer4ChLightOutput",
                                                    light.LightOutput)
 
 CONFIG_SCHEMA = (
-    cv.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
+    BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
         {
             cv.Required(CONF_CHANNEL): cv.hex_int_range(0, 255),
         }))

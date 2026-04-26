1d2711299d1e6e9de882e69cab52f7f7fb5084a8
Tero Kivinen <kivinen@iki.fi>
2026-04-26 03:20:42 +0300

    .

--- components/i2c_dimmer_4ch/light.py
+++ components/i2c_dimmer_4ch/light.py
@@ -3,7 +3,6 @@ from esphome.components import light, output
 from esphome.components import i2c
 from esphome.components.i2c import I2CBus
 import esphome.config_validation as cv
-from esphome.components.light import  BRIGHTNESS_ONLY_LIGHT_SCHEMA
 from esphome.const import (
     CONF_OUTPUT_ID,
     CONF_CHANNEL,
@@ -16,25 +15,21 @@ MULTI_CONF = True
 
 i2c_dimmer_4ch_ns = cg.esphome_ns.namespace("i2c_dimmer_4ch")
 I2CDimmer4ChLightOutput = i2c_dimmer_4ch_ns.class_("I2CDimmer4ChLightOutput",
-                                                   light.LightOutput)
+                                                   light.LightOutput,
+                                                   cg.Component, i2c.I2CDevice)
 
 CONFIG_SCHEMA = (
-    BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
+    light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
         {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(I2CDimmer4ChLightOutput),
             cv.Required(CONF_CHANNEL): cv.hex_int_range(0, 255),
-        }))
-
-#light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
-#        {
-#            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(I2CDimmer4ChLightOutput),
-#            cv.Required(CONF_CHANNEL): cv.hex_int_range(0, 255),
-#            cv.Optional(CONF_MIN_BRIGHTNESS, default=0): cv.uint8_t,
-#            cv.Optional(CONF_MAX_BRIGHTNESS, default=100): cv.uint8_t,
-#        }
-#    )
-#    .extend(cv.COMPONENT_SCHEMA)
-#    .extend(i2c.i2c_device_schema(0x3f))
-#)
+            cv.Optional(CONF_MIN_BRIGHTNESS, default=0): cv.uint8_t,
+            cv.Optional(CONF_MAX_BRIGHTNESS, default=100): cv.uint8_t,
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x3f))
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
